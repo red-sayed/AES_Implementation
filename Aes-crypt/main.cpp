348c739e756790430cfb7558654ade5665923140
Vladimir Rogozin <vladimir20040609@gmail.com>
2022-04-23 01:00:00 +0300

    crypt

--- Aes-crypt/main.cpp
+++ Aes-crypt/main.cpp
@@ -112,22 +112,58 @@ int main() {
             std::cout << "[\033[93mAes\033[0m/\033[94m" + mode + "\033[0m] \033[4mEncrypted message(hex):\033[0m";
 
             if (mode == "CBC-128") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesCBC128(msg, key, iv));
+                std::string *res1 = Red::EncryptAesCBC128(msg, key, iv);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "CBC-192") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesCBC192(msg, key, iv));
+                std::string *res1 = Red::EncryptAesCBC192(msg, key, iv);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "CBC-256") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesCBC256(msg, key, iv));
+                std::string *res1 = Red::EncryptAesCBC256(msg, key, iv);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-128") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesECB128(msg, key));
+                std::string *res1 = Red::EncryptAesECB128(msg, key);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-192") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesECB192(msg, key));
+                std::string *res1 = Red::EncryptAesECB192(msg, key);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-256") {
-                std::cout << std::endl << std::endl << *Red::GetHexArray(*Red::EncryptAesECB256(msg, key));
+                std::string *res1 = Red::EncryptAesECB256(msg, key);
+                std::string *res2 = Red::GetHexArray(*res1);
+
+                std::cout << std::endl << std::endl << *res2;
+
+                delete res1;
+                delete res2;
             }
 
             std::cout << std::endl << std::endl << std::endl << "Press Enter to continue.";
@@ -145,28 +181,58 @@ int main() {
             std::cout << std::endl << std::endl;
 
             if (mode == "CBC-128") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesCBC128(*Red::GetStrArray(msg), key, iv);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesCBC128(*res1, key, iv);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "CBC-192") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesCBC192(*Red::GetStrArray(msg), key, iv);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesCBC192(*res1, key, iv);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "CBC-256") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesCBC256(*Red::GetStrArray(msg), key, iv);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesCBC256(*res1, key, iv);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-128") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesECB128(*Red::GetStrArray(msg), key);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesECB128(*res1, key);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-192") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesECB192(*Red::GetStrArray(msg), key);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesECB192(*res1, key);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
 
             } else if (mode == "ECB-256") {
-                std::string a = *Red::GetStrArray(msg.c_str());
-                std::cout << *Red::DecryptAesECB256(*Red::GetStrArray(msg), key);
+                std::string *res1 = Red::GetStrArray(msg.c_str());
+                std::string *res2 = Red::DecryptAesECB256(*res1, key);
+
+                std::cout << *res2;
+
+                delete res1;
+                delete res2;
             }
 
             std::cout << std::endl << std::endl << std::endl << "Press Enter to continue.";
